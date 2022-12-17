76affbec42a2e4e2feed694953469f392129c119
Stephen Leach <sfkleach@gmail.com>
2022-12-17 16:58:20 +0000

    Day 16 Part 1

---
+++ day16/elephants.py
@@ -0,0 +1,161 @@
+import re
+from collections import deque
+from queue import PriorityQueue
+import heapq
+
+class Valve:
+
+    def __init__( self, name, rate, neighbours ):
+        self.name = name                    # public
+        self.rate = rate                    # public
+        self.neighbours = neighbours        # public
+
+    def __repr__( self ):
+        return f'<{self.name} rate={self.rate}>'
+
+class SearchStatus:
+
+    def __init__( self, valve, visited, unopened_valves, countdown, score ):
+        self._countdown = countdown
+        self._at = valve
+        self._visited = visited
+        self._unopened = unopened_valves
+        self._score = score
+        self._best_possible = None
+
+    def _getCountdown( self ):
+        n = self._countdown - 1
+        while n > 0:
+            yield n
+            n -= 2
+
+    def bestPossible( self ):
+        # print( *self._unopened.items() )
+        # for ( m, v ) in zip( self._getCountdown(), self._unopened.values() ):
+        #     print( f'{m} * {v.rate} -> {m * v.rate}' )
+        if self._best_possible is None:
+            self._best_possible = self._score + sum( m * v.rate for ( m, v ) in zip( self._getCountdown(), self._unopened.values() ) )
+        return self._best_possible
+
+    def __lt__( self, other ):
+        return self._score < other._score
+
+    def __le__( self, other ):
+        return self._score <= other._score
+
+    def __gt__( self, other ):
+        return self._score > other._score
+
+    def __ge__( self, other ):
+        return self._score >= other._score
+
+    def __eq__( self, other ):
+        return self._score == other._score
+
+    def __ne__( self, other ):
+        return self._score != other._score
+
+    def __lt__( self, other ):
+        return self._score < other._score
+
+    def isSolution( self ):
+        return self._countdown <= 0
+
+    def score( self ):
+        return self._score
+
+    def __repr__( self ):
+        return f'<{self._at.name} score={self._score} count={self._countdown} visited={self.visited()}>'
+
+    def _genVisited( self ):
+        linked_list = self._visited
+        while linked_list:
+            ( t, rest ) = linked_list
+            linked_list = rest
+            yield t
+
+    def visited( self ):
+        return tuple( self._genVisited() )
+
+    def options( self ):
+        if self._countdown <= 0:
+            return
+        new_countdown = self._countdown - 1
+        if self._at.name in self._unopened and self._at.rate > 0:
+            # Open valve.
+            # print( 'open' )
+            new_unopened = self._unopened.copy()
+            del new_unopened[ self._at.name ]
+            new_score = self._score + self._at.rate * max( 0, self._countdown - 1 )
+            # print( 'score', new_score )
+            new_at = self._at
+            new_visited = ( ( "open", self._at ), self._visited )
+            yield SearchStatus( new_at, new_visited, new_unopened, new_countdown, new_score )
+        for n in self._at.neighbours:
+            # Move to a neighbour.
+            # print( 'move' )
+            new_unopened = self._unopened
+            new_score = self._score
+            new_at = n
+            new_visited = ( ( "move", new_at ), self._visited )
+            yield SearchStatus( new_at, new_visited, new_unopened, new_countdown, new_score )
+
+class Search:
+
+    def __init__( self, start, valves ):
+        self._Q = [ SearchStatus( start, None, valves, 30, 0 ) ]
+        self._high_score = 0
+        self._best = None
+
+    def canContinue( self ):
+        return bool( self._Q )
+
+    def check( self ):
+        s = self._Q.pop()
+        if s.isSolution():
+            if s.score() > self._high_score:
+                self._high_score = s.score()
+                self._best = s
+                # print( 'SOLUTION', s )
+        else:
+            if s.bestPossible() > self._high_score:
+                n = len( self._Q )
+                for t in s.options():
+                    if t.bestPossible() > self._high_score:
+                        self._Q.append( t )
+                if len( self._Q ) > n:
+                    self._Q.sort( key=lambda s: s.score() )
+
+    def highScore( self ):
+        return self._high_score
+
+
+class Cave:
+
+    def __init__( self, valve_map ):
+        self._valves = valve_map
+
+    def newSearch( self, start ):
+        return Search( self._valves[start], self._valves )
+
+def newCave( valve_tuples ):
+    valves = tuple( valve_tuples )
+    valve_map = {}
+    for ( name, rate, neighbours ) in sorted( valves, key=lambda x: int(x[1]), reverse=True ):
+        # print( 'add', name, rate )
+        valve_map[ name ] = Valve( name, int( rate ), [] )
+    for ( name, rate, neighbours ) in valves:
+        valve_map[ name ].neighbours = sorted( tuple( valve_map[ n ] for n in neighbours ), key=lambda v: v.rate ) 
+    return Cave( valve_map )
+
+def readValves( file ):
+    for line in file:
+        if m := re.match( r'Valve ([A-Z]+) has flow rate=([0-9]+); tunnel[s]? lead[s]? to valve[s]? (.*)$', line ):
+            yield ( m[1], int(m[2]), m[3].replace( ',', ' ' ).split() )
+        else:
+            raise Exception( 'BAD INPUT ' + line )
+
+def readElephantFile( fname ):
+    with open( fname, 'r' ) as file:
+        return newCave( readValves( file ) )
+
---
+++ day16/part1.py
@@ -0,0 +1,11 @@
+from elephants import *
+
+def part1( fname ):
+    S = readElephantFile( fname ).newSearch( 'AA' )
+    while S.canContinue():
+        S.check()
+    print( S.highScore() )
+
+if __name__ == "__main__":
+    # part1( 'test.txt' )     # 1651
+    part1( 'input.txt' )    # 2183
